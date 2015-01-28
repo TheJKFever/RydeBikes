2df752c9b85c98ff4fdb13dc7ecab23e6d0183e6
Jon Koehmstedt <Jon.Koehmstedt@gmail.com>
2015-01-27 20:19:37 -0800

    removed question marks from single line ifs

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,12 +3,12 @@ class BikesController < ApplicationController
 	before_filter :authenticate_admin!, :except => [:index, :show]
   before_action :set_bike, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html, :json
+  respond_to :json
 
   def index # returns all bikes within a certain radius
     # defaults raius to half mile, and coordinate to requesting ip address
-    radius = if params[:radius] ? params[:radius] : 0.5
-    coordinates = if params[:coordinates] ? params[:coordinates] : request.location
+    radius = params[:radius] ? params[:radius] : 0.5
+    coordinates = params[:coordinates] ? params[:coordinates] : request.location
     @bikes = Coordinate.near(coordinates, radius).joins(:bikes)
     respond_with(@bikes)
   end
@@ -47,6 +47,7 @@ class BikesController < ApplicationController
   end
 
   def bike_params
-    params.require(:bike).permit(:status, :model, :network => [:name], :location => [:latitude, :longitude])
+    params.require(:bike)
+    .permit(:status, :model, :network => [:name], :location => [:latitude, :longitude])
   end
 end
