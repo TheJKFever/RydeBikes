01d68ffec54168e426b67be2f09b6653a9330439
thejkfever <Jon.Koehmstedt@gmail.com>
2015-06-05 00:30:55 -0500

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,14 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.default_url_options = { :host => 'ryde.herokuapp' }
+  ActionMailer::Base.smtp_settings = {  
+    :address              => "smtp.gmail.com",  
+    :port                 => 587,  
+    :domain               => "gmail.com",  
+    :user_name            => "jon.koehmstedt@gmail.com",  
+    :password             => "J3rim1Ah2911",
+    :authentication       => "plain"
+  }   
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
